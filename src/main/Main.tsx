1595b377ce1d9c727c58f7728e52d2199292c0d3
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-05-11 23:35:41 +0400

    portfolio 4

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -6,6 +6,7 @@ const Main = () => {
     return (
         <div className={styles.main}>
             <div className={styles.text}>
+            <div className={styles.container}></div>
                 <span>Hi there!</span>
                 <h1>I am Andrii Kulchytskyi</h1>
                 <p>A React Developer</p>
