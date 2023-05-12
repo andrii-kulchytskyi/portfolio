9363d2a50fe93fa5d53fa11ea07a929fdf2fb33d
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-05-12 18:57:27 +0400

    portfolio 4

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import styles from '../main/Main.module.css'
+import styles from '../main/Main.module.css';
+import stylesContainer from '../common/styles/Container.module.css';
 
 
 const Main = () => {
     return (
         <div className={styles.main}>
             <div className={styles.text}>
-            <div className={styles.container}></div>
+                <div className={stylesContainer.container}></div>
                 <span>Hi there!</span>
                 <h1>I am Andrii Kulchytskyi</h1>
                 <p>A React Developer</p>
