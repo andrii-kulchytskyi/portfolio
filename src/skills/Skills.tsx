f37d88d6ea807609b1a13ccd056dd045d5c8a3a7
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-05-17 22:53:23 +0400

    portfolio 5

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import style from'./Skills.module.css'
+import style from './Skills.module.css'
 
 const Skills = () => {
     return (
         <div className={style.skillsBlock}>
+            <h2 className={style.title}>Skills
+                <div className={style.skills}></div>
+            </h2>
 
         </div>
     );
